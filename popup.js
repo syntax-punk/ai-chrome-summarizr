7fbdb5f740ce2a5dd37150f873f74cf5d0bdfc6b
theVoogie <david.jaeren@webstep.no>
2023-07-31 16:24:45 +0200

    Changed manifest and updated the logic a bit

--- popup.js
+++ popup.js
@@ -1,11 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+
+    const activeTabId = tabs[0].id;
     chrome.scripting.executeScript(
       {
-        target: { tabId: tabs[0].id },
+        target: { tabId: activeTabId },
         func: getText,
       },
       function (results) {
+        console.log(results)
         const text = results[0].result;
         document.getElementById('text').textContent = text;
       }
